7113450b7d2efd7b9d6150f3a3511d80cbf31097
João Marcelo Gonçalves Freire <39954565+joaossa@users.noreply.github.com>
2019-04-02 15:55:12 -0300

    Update Plugin.php
    Change task name to TaskSendConclusionEmail

--- Plugin.php
+++ Plugin.php
@@ -3,12 +3,12 @@
 namespace Kanboard\Plugin\AutomaticAction;
 
 use Kanboard\Core\Plugin\Base;
-use Kanboard\Plugin\AutomaticAction\Action\TaskRename;
+use Kanboard\Plugin\AutomaticAction\Action\TaskSendConclusionEmail;
 
 class Plugin extends Base
 {
     public function initialize()
     {
-        $this->actionManager->register(new TaskRename($this->container));
+        $this->actionManager->register(new TaskSendConclusionEmail($this->container));
     }
 }

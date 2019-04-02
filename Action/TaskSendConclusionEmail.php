fc371ad53759d30df76041048576c3a1f1bbd2c5
João Marcelo Gonçalves Freire <39954565+joaossa@users.noreply.github.com>
2019-04-02 16:05:26 -0300

    Update TaskSendConclusionEmail.php

--- Action/TaskSendConclusionEmail.php
+++ Action/TaskSendConclusionEmail.php
@@ -1,1 +1,81 @@
-
+<?php
+namespace Kanboard\Plugin\AutomaticAction\Action;
+use Kanboard\Model\TaskModel;
+use Kanboard\Action\Base;
+/**
+ * Rename Task Title
+ *
+ * @package action
+ * @author  Joao Freire
+ */
+class TaskSendConclusionEmail extends Base
+{
+    /**
+     * Get automatic action description
+     *
+     * @access public
+     * @return string
+     */
+    public function getDescription()
+    {
+        return t('Send conclusion default e-mail message when the task is moved to - feito - column');
+    }
+    /**
+     * Get the list of compatible events
+     *
+     * @access public
+     * @return array
+     */
+    public function getCompatibleEvents()
+    {
+        return array(
+            TaskModel::EVENT_MOVE_COLUMN,
+        );
+    }
+    /**
+     * Get the required parameter for the action (defined by the user)
+     *
+     * @access public
+     * @return array
+     */
+    public function getActionRequiredParameters()
+    {
+        return array(
+            'title' => t('Title'),
+        );
+    }
+    /**
+     * Get the required parameter for the event
+     *
+     * @access public
+     * @return string[]
+     */
+    public function getEventRequiredParameters()
+    {
+        return array(
+            'task_id',
+        );
+    }
+    /**
+     * Execute the action
+     *
+     * @access public
+     * @param  array   $data   Event data dictionary
+     * @return bool            True if the action was executed or false when not executed
+     */
+    public function doAction(array $data)
+    {
+        return $this->taskModificationModel->update(array('id' => $data['task_id'], 'title' => $this->getParam('title')));
+    }
+    /**
+     * Check if the event data meet the action condition
+     *
+     * @access public
+     * @param  array   $data   Event data dictionary
+     * @return bool
+     */
+    public function hasRequiredCondition(array $data)
+    {
+        return true;
+    }
+}
